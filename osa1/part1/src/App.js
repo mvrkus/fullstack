b144b1009996f900886f1736c6986cda17ee98fb
mvrkus <markus.tarkkala@gmail.com>
2022-11-04 10:57:07 +0200

    1.1

---
+++ osa1/part1/src/App.js
@@ -0,0 +1,45 @@
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course = {course} />
+      <Content name={part1} exercises = {exercises1}/>
+      <Content name={part2} exercises = {exercises2}/>
+      <Content name={part3} exercises = {exercises3}/>
+      <Total total={exercises1 + exercises2 + exercises3}/>
+    </div>
+  )
+}
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <p>{props.name} {props.exercises}</p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Total number of exercises {props.total} </p>
+    </div>
+  )
+}
+
+export default App
