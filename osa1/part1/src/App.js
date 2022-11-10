113b6f716cc03663df4eac1387ccb3c47a4b1c28
mvrkus <markus.tarkkala@gmail.com>
2022-11-10 14:43:01 +0200

    1.5

--- osa1/part1/src/App.js
+++ osa1/part1/src/App.js
@@ -1,40 +1,45 @@
+import { getValue } from "@testing-library/user-event/dist/utils"
+
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
       <Header course={course}/>
-      <Content parts={parts}/>
-      <Total parts={parts}/>
+      <Content course={course}/>
+      <Total course={course}/>
     </div>
   )
 }
+
 const Header = (props) => {
   console.log(props)
-  return <h1>{props.course}</h1>
+  return <h1>{props.course.name}</h1>
 }
 
 const Content = (props) => {
   console.log(props)
   return (
     <div>
-      <Part part={props.parts[0]}/>
-      <Part part={props.parts[1]}/>
-      <Part part={props.parts[2]}/>
+      <Part part={props.course.parts[0]}/>
+      <Part part={props.course.parts[1]}/>
+      <Part part={props.course.parts[2]}/>
     </div>
   )
 }
@@ -50,9 +55,10 @@ const Part = (props) => {
 
 const Total = (props) => {
   console.log(props)
+  
   return (
     <div>
-      <p>Total exercises {props.total}</p>
+      <p>Total exercises {props.course.parts[0].exercises+props.course.parts[1].exercises+props.course.parts[2].exercises}</p>
     </div>
   )
 }
