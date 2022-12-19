70279b99d4972894a2777762d582315e4f58db10
mvrkus <markus.tarkkala@gmail.com>
2022-12-19 16:31:16 +0200

    2.1

---
+++ osa2/kurssitiedot/src/App.js
@@ -0,0 +1,68 @@
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  )
+}
+
+const Header = ({ name }) => {
+  return (
+    <div>
+      <h1>{name}</h1>
+    </div>
+  )
+}
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      <ul>
+        {parts.map(part =>
+           <Part key={part.id} part={part} /> 
+        )}
+        
+      </ul>
+    </div>
+  )
+}
+
+const Part = ({ part }) => {
+  return (
+    <li>{part.name} {part.exercises}</li>
+  )
+}
+
+
+export default App
