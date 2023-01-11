0ea0a70977de73d1f9fcac08e0292d8362ec62d2
mvrkus <markus.tarkkala@gmail.com>
2023-01-11 14:15:14 +0200

    2.8

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -4,37 +4,49 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas'
+    { name: 'Arto Hellas',
+      number: '040-1231244'
     }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
   
 
   const addName = (event) => {
     event.preventDefault()
     
-    if(persons.some(checkName) === false){
+    if(persons.some(checkName) === false && newName != ''){
       
       const personObject = {
-        name: newName
+        name: newName,
+        number: newNumber
         }
       setPersons(persons.concat(personObject))
       setNewName('')
+      setNewNumber('')
       return
     }
 
     else {
-      alert(`Sinulla on jo ystävä nimeltä ${newName}. Älä tutustu uuteen.`)
-      return
+      if (newName === '') {
+        alert('Numerolla ei ole nimeä')
+        return
+      }
+      else {
+        alert(`Sinulla on jo ystävä nimeltä ${newName}. Älä tutustu uuteen.`)
+        return
+      }
     }
   }
 
   const checkName = (person) => newName === person.name;
 
   const handleNewName = (event) => {
-    
     setNewName(event.target.value)
-    
+  }
+
+  const handleNewNumber = (event) => {
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -43,6 +55,9 @@ const App = () => {
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleNewName}/>
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNewNumber}/>
         </div>
         <div>
           <button type="submit">add</button>
@@ -61,7 +76,7 @@ const App = () => {
 
 const Person = ({ person }) => {
   return (
-    <li>{person.name}</li>
+    <li>{person.name} {person.number}</li>
   )
 }
 
